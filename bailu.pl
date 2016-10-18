23827eb84e0f3fa13c4a41c230a79fb686648718
Yijing Sun <reacherone2000@hotmail.com>
2016-10-18 12:02:22 -0700

    archived the previous work
    Previous work on biquge and bailu com

---
+++ bailu.pl
@@ -0,0 +1,77 @@
+﻿#Version 1.0 for oklink.net
+use LWP::Simple qw(getstore);
+use Encode;
+
+getstore( "http://www.oklink.net/wxsj/jing-yong/lean-on-sky/index.html", "toc.html" );
+print "Content Download Completed\n";
+open (source, "<", "toc.html") || die ("Could not open content file");
+open (content, ">", "toc.ncx") || die ("Could not write content file");
+print content ("  \<docTitle\>
+    \<text\>title of book\<\/text\>
+  \<\/docTitle\>");
+while (<source>){
+	#if(/HREF=\"\d(\d\d).htm\"\>[\x80-\xFF]{2}\d\d[\x80-\xFF]{2}[\x80-\xFF]{2}(.*?)\<\/a\> \<\/td\>/){
+	if(/HREF=\"0(\d\d).htm\"\>(.*?)\<\/a\> \<\/td\>/){
+		@array = (@array, $1,$2);}
+	}
+	while(@array){
+		$i++;
+		$num = shift @array;
+		$name = shift @array;
+		print "$name\n";
+		if($i>0){
+		getstore( "http:\/\/www\.oklink\.net\/wxsj\/jing\-yong\/lean\-on\-sky\/0$num.htm", "$i.htm" );
+		open (htmlsource, "<", "$i.htm") || die ("Could not open source file");
+		open (htmlresult, ">$i.xhtml") || die ("Could not write result file");
+		$para = 0;
+		print htmlresult ("<\?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"\?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
+  \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
+
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head>
+  <title></title>
+</head>
+
+<body>");
+		while (<htmlsource>){
+			if(/\<td\>\<pre\>\<span class\=\"content\"\>/){
+				$content = 1;}
+			if(/\<\/center\>\<\/div\>/){
+				$content = 0;
+				print htmlresult "\<\/p\>\n"}
+			if($content){
+				if($para){
+					if(/    ([\x80-\xFF]{2}.*[\x80-\xFF]{2})/){
+						$temp = encode("utf-8",decode("gbk",$1));
+						print htmlresult "\<\/p\>\n
+  \<p\>\&nbsp\; \&nbsp\; $temp";}
+					if(/([\x80-\xFF]{2}.*[\x80-\xFF]{2})/){
+						$temp = encode("utf-8",decode("gbk",$1));
+						print htmlresult $temp;}
+						next;
+					next;}
+				elsif(/    ([\x80-\xFF]{2}.*[\x80-\xFF]{2})/){
+					$temp = encode("utf-8",decode("gbk",$1));
+					print htmlresult "
+  \<p\>\&nbsp\; \&nbsp\; $temp";
+					$para = 1;
+					next;}}
+			next;}
+		print htmlresult ("\<\/body\>
+\<\/html\>");
+		close (htmlsource);
+		close (htmlresult);}
+		$num =~ s/\b0+//g;
+		print content ("\<navMap\>
+    \<navPoint id\=\"navPoint\-$num\" playOrder\=\"$num\"\>
+      \<navLabel\>
+        \<text\>$name\<\/text\>
+      \<\/navLabel\>
+      \<content src\=Text\/\"$i.xhtml\"\/\>
+    \<\/navPoint\>");
+	next;}
+	print content ("
+	\<\/navMap\>
+\<\/ncx\>");
+	close (content);
---
+++ biquge.pl
@@ -0,0 +1,147 @@
+﻿#Perl Script for biquge.la
+use LWP::Simple qw(getstore);
+use Encode;
+
+#Global Parameters:
+$bookid=61;
+
+#Grab Table of Content Page
+getstore( "http://www.biquge.la/book/$bookid", "toc.html" );
+print "Table of Content Download Completed\n";
+
+#Create .ncx Table File
+open (tocsource, "<", "toc.html") || die ("Could not open content file");
+open (toctarget, ">", "toc.ncx") || die ("Could not create content file");
+print toctarget ("  \<docTitle\>
+    \<text\>title of book\<\/text\>
+  \<\/docTitle\>
+  \<navMap\>");
+
+#Read Chapter num & name from Content
+$j=0;
+while (<tocsource>){
+	if(/\<dd\>\<a href\=\"(.*)\.html\"\>(.*?)\<\/a\>\<\/dd\>\<dd\>\<a href\=\"(.*)\.html\"\>(.*?)\<\/a\>\<\/dd\>\<dd\>\<a href\=\"(.*)\.html\"\>(.*?)\<\/a\>\<\/dd\>/){
+		@num = (@num, $1, $3, $5);
+		@name = (@name, $2, $4, $6);
+		$j = $j + 3;}
+	elsif(/\<dd\>\<a href\=\"(.*)\.html\"\>(.*?)\<\/a\>\<\/dd\>\<dd\>\<a href\=\"(.*)\.html\"\>(.*?)\<\/a\>\<\/dd\>/){
+			@num = (@num, $1, $3);
+			@name = (@name, $2, $4);
+			$j = $j + 2;}
+	elsif(/\<dd\>\<a href\=\"(.*)\.html\"\>(.*?)\<\/a\>\<\/dd\>/){
+			@num = (@num, $1);
+			@name = (@name, $2);
+			$j = $j + 1;}
+	}
+	
+print ("$j\n");
+close(tocsource);
+
+#Processing Content
+$i++;
+$num = shift @num;
+$name = shift @name;
+print $name;
+while(@num){
+	if($i>0){
+		getstore( "http:\/\/www\.biquge\.la\/book\/$bookid\/$num.html", "$i.html" );
+		print "$i\n";
+		open (htmlsource, "<", "$i.html") || redo;
+		open (htmlresult, ">$i.xhtml");
+		while (<htmlsource>){
+			if(/\<div id\=\"content\"\>(.*)\<\/div\>/){
+				$temp = encode("utf-8",decode("gbk",$1));
+				$temp =~ s/\&nbsp\;\&nbsp\;\&nbsp\;\&nbsp\;\;//g;
+				$temp =~ s/\&nbsp\;\&nbsp\;\&nbsp\;\&nbsp\;/
+  \<p\>\&nbsp\; \&nbsp\; /g;
+				$temp =~ s/\<br.*?\>\<br.*?\>/\<\/p\>\n/g;
+				$temp =~ s/rì/日/g;
+				$temp =~ s/chūn/春/g;
+				$temp =~ s/sè/色/g;
+				$temp =~ s/xìng/性/g;
+				$temp =~ s/dú lì/独立/g;
+				$temp =~ s/shè/射/g;
+				$temp =~ s/yīn/阴/g;
+				$temp =~ s/zhōng yāng/中央/g;
+				$temp =~ s/cāo/操/g;
+				$temp =~ s/sāo/骚/g;
+				print htmlresult ("<\?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"\?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
+  \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
+
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head>
+  <title></title>
+</head>
+
+<body>");
+				print htmlresult $temp;
+				print htmlresult ("\<\/body\>
+\<\/html\>");}
+		}
+		close (htmlsource);
+		close (htmlresult);
+	}
+	print toctarget ("
+\<navPoint id\=\"navPoint\-$i\" playOrder\=\"$i\"\>
+  \<navLabel\>
+    \<text\>$name\<\/text\>
+  \<\/navLabel\>
+  \<content src\=\"Text\/$i.xhtml\"\/\>
+\<\/navPoint\>");
+	$i++;
+	$num = shift @num;
+	$name = shift @name;
+	print $name;
+	next;
+}
+#Final Chapter
+getstore( "http:\/\/www\.biquge\.la\/book\/$bookid\/$num.html", "$i.html" );
+print "$i\n";
+open (htmlsource, "<", "$i.html") || die ("Could not open file");
+open (htmlresult, ">$i.xhtml");
+while (<htmlsource>){
+	if(/\<div id\=\"content\"\>(.*)\<\/div\>/){
+		$temp = encode("utf-8",decode("gbk",$1));
+		$temp =~ s/\&nbsp\;\&nbsp\;\&nbsp\;\&nbsp\;\;//g;
+		$temp =~ s/\&nbsp\;\&nbsp\;\&nbsp\;\&nbsp\;/
+  \<p\>\&nbsp\; \&nbsp\; /g;
+		$temp =~ s/\<br.*?\>\<br.*?\>/\<\/p\>\n/g;
+		$temp =~ s/rì/日/g;
+		$temp =~ s/chūn/春/g;
+		$temp =~ s/sè/色/g;
+		$temp =~ s/xìng/性/g;
+		$temp =~ s/dú lì/独立/g;
+		$temp =~ s/shè/射/g;
+		$temp =~ s/yīn/阴/g;
+		$temp =~ s/zhōng yāng/中央/g;
+		$temp =~ s/cāo/操/g;
+		$temp =~ s/sāo/骚/g;
+		print htmlresult ("<\?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"\?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
+  \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
+
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head>
+  <title></title>
+</head>
+
+<body>");
+		print htmlresult $temp;
+		print htmlresult ("\<\/body\>
+\<\/html\>");}
+}
+close (htmlsource);
+close (htmlresult);
+print toctarget ("
+\<navPoint id\=\"navPoint\-$i\" playOrder\=\"$i\"\>
+  \<navLabel\>
+    \<text\>$name\<\/text\>
+  \<\/navLabel\>
+  \<content src\=\"Text\/$i.xhtml\"\/\>
+\<\/navPoint\>");
+#End of Table
+print toctarget ("
+	\<\/navMap\>
+\<\/ncx\>");
+close (toctarget);
